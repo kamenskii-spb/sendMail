decbd44aa92460e00003c72e40fcbb2658beaa02
Dimak <gxiv@ya.ru>
2020-09-16 10:41:31 +0300

    make it better

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,7 +1,48 @@
+const nodemailer = require('nodemailer')
+const keys = require('../keys')
+
 module.exports.send = async function (req, res) {
     try {
-      return req.ipInfo
+    
+      const emailSetting = {
+        host: 'smtp.mail.ru',
+        port: 465,
+        secure: true, // use TLS
+        auth: {
+          user: keys.EMAIL,
+          pass: keys.EMAIL_PASSWORD
+        },
+        tls: {
+          // do not fail on invalid certs
+          rejectUnauthorized: false
+        }
+      }
+      
+      const domen = keys.BASE_URL
 
+
+
+
+
+      const transporter = nodemailer.createTransport(emailSetting);
+
+      const mailOptions = {
+        from: keys.EMAIL,
+        to: 'gxiv@ya.ru',
+        subject: 'Тест',
+        text: `
+          
+          Тестовое письмо от сервера ${keys.BASE_URL}
+          `
+      };
+    
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log(error);
+        }
+      });
+
+      return  res.status(200).json(keys.EMAIL)
     } catch (error) {
       console.log(error)
     }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 const port = process.env.PORT ||  8080
 
+
 app.listen(port, ()=>{
     console.log(`server has been started ${port}`)
 });
