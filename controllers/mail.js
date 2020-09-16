c7a9b58d49bc86d69116037816add3accbece2ee
Dimak <gxiv@ya.ru>
2020-09-16 12:04:47 +0300

    make it better12

--- controllers/mail.js
+++ controllers/mail.js
@@ -11,44 +11,44 @@ module.exports.send = async function (req, res) {
 
 
     
-      const emailSetting = {
-        host: 'smtp.mail.ru',
-        port: 465,
-        secure: true, // use TLS
-        auth: {
-          user: keys.EMAIL,
-          pass: keys.EMAIL_PASSWORD
-        },
-        tls: {
-          // do not fail on invalid certs
-          rejectUnauthorized: false
-        }
-      }
+      // const emailSetting = {
+      //   host: 'smtp.mail.ru',
+      //   port: 465,
+      //   secure: true, // use TLS
+      //   auth: {
+      //     user: keys.EMAIL,
+      //     pass: keys.EMAIL_PASSWORD
+      //   },
+      //   tls: {
+      //     // do not fail on invalid certs
+      //     rejectUnauthorized: false
+      //   }
+      // }
       
-      const domen = keys.BASE_URL
+      // const domen = keys.BASE_URL
 
 
 
 
 
-      const transporter = nodemailer.createTransport(emailSetting);
+      // const transporter = nodemailer.createTransport(emailSetting);
 
-      const mailOptions = {
-        from: keys.EMAIL,
-        to: 'bestgift-spb@yandex.ru',
-        subject: 'Тест',
-        text: `
-          Тестовое письмо от сервера ${domen}
-          `
-      };
+      // const mailOptions = {
+      //   from: keys.EMAIL,
+      //   to: 'bestgift-spb@yandex.ru',
+      //   subject: 'Тест',
+      //   text: `
+      //     Тестовое письмо от сервера ${domen}
+      //     `
+      // };
     
-      transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-          console.log(error);
-        }
-      });
+      // transporter.sendMail(mailOptions, function(error, info) {
+      //   if (error) {
+      //     console.log(error);
+      //   }
+      // });
 
-      return  res.status(200).json({ppppppp: keys.BASE_URL })
+      return  res.status(200).json({ppppppp: keys})
     } catch (error) {
       return  res.status(200).json({error: keys.BASE_URL })
     }
